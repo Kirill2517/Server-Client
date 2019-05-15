3343654b70b13b7d4c5309d1c4231c58c0c76ce6
Kirill2517 <49959794+Kirill2517@users.noreply.github.com>
2019-05-15 18:07:56 +0300

    Добавьте файлы проекта.

---
+++ Lesson18/StringToFormula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson18
+{
+    public static class Calculator
+    {
+        // Создадим статический экземпляр DataTable, чтобы каждый раз не инициализировать его заново
+        private static DataTable Table { get; } = new DataTable();
+
+        // Наш метод подсчета
+        // Добавьте отлов ошибок по вкусу)
+        public static double Calc(string Expression) => Convert.ToDouble(Table.Compute(Expression, string.Empty));
+    }
+}
